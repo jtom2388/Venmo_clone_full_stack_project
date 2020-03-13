2dda45c23156c1829a8b59e16560e839ecde8fbf
jordantom <jordan.tom88@gmail.com>
2020-03-12 19:21:18 -0700

    remove logger from app

--- pay_up/frontend/store/store.js
+++ pay_up/frontend/store/store.js
@@ -4,7 +4,7 @@ import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 
 const configureStore = (preloadedState = {}) => {
-    return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+    return createStore(rootReducer, preloadedState, applyMiddleware(thunk))
 }
 
 export default configureStore;
